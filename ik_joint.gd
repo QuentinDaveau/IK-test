30b7848ba6a34feb62e9f6338f48ab5e6134d82d
Quentin <daveau.quentin@hotmail.fr>
2022-01-29 15:30:31 +0100

    Cleanup IK coded and project

--- ik_joint.gd
+++ ik_joint.gd
@@ -9,4 +9,4 @@ Simple data structure to hold properties of an IK joint
 export(NodePath) var target_transform
 
 # Bend angle constraints in euler angles (relative to the forward of the joint)
-export(Vector3) var constraints = Vector3.ZERO
+export(float, 0.0, 180) var constraint = 0.0
--- simple_ik.gd
+++ simple_ik.gd
@@ -1,22 +1,19 @@
 extends Node
 
+
 """
-Simple and ugly 3D IK script based on the FABRIK algorithm
+Very basic IK class with simple, one-axis constraints.
 """
-# TODO: Add weight and joint constraints
 
 
-
-export(int) var _max_resolves: int = 6
+export(int) var _max_resolves: int = 4 # A resolve is a forward and backward pass
 export(float) var _min_accepted_dist: float = 0.1
-
-export(Array, NodePath) var _joints_paths: Array = []
 export(NodePath) var _target_path
 
-# Those variables are exported to allow to serialize them and joints_pos the IK in the inspector
-var _joints: Array = []
-var _joints_dists: Array = []
-var _target: Spatial
+# Array of IkJoint
+export(Array, Resource) var _joints
+
+var _joints_resolvers := []
 
 
 
@@ -25,57 +22,114 @@ func _ready() -> void:
 
 
 
-func _process(delta):
-	_process_ik()
+func _process(delta: float) -> void:
+	_resolve_ik()
 
 
 
-func _process_ik() -> void:
-	var joints_pos := [_joints[0].global_transform.origin]
-	for joint in _joints:
-		joints_pos.append(joint.global_transform.origin)
-	
+func _resolve_ik() -> void:
+	_refresh_resolvers()
+	# Resolving the IK
 	for i in range(_max_resolves):
-		var return_pass := i % 2 == 0
-		var coeff := 1 if return_pass else -1
-		var dist_coeff := 1 if return_pass else 0
-		for j in range(joints_pos.size() - 2 if return_pass else 1, 0 if return_pass else joints_pos.size() - 1, -1 if return_pass else 1):
-			var dir: Vector3 = joints_pos[j + coeff].direction_to(joints_pos[j])
-			joints_pos[j] = joints_pos[j + coeff] + dir * _joints_dists[j + dist_coeff]
-		
-		if not return_pass and joints_pos[-2].distance_to(_target.global_transform.origin) <= _min_accepted_dist:
+		if _ik_pass():
 			break
-		
-	_apply_to_transforms(joints_pos)
+	_apply_to_joints()
 
 
 
-func _apply_to_transforms(var positions: Array) -> void:
-	for r in range(1, positions.size() - 2):
-		_joints[r - 1].global_transform.origin = positions[r]
-		_joints[r - 1].global_transform.basis = _joints[r - 1].global_transform.looking_at(positions[r + 1], Vector3.UP).basis
+func _refresh_resolvers() -> void:
+	for resolver in _joints_resolvers:
+		resolver.refresh_position()
+
+
+# Resolves the IK. Returns true if close enough to target
+func _ik_pass() -> bool:
+	# Forward pass
+	for j in range(_joints_resolvers.size() - 2, 0, -1):
+		_joints_resolvers[j].resolve_position(_joints_resolvers[j + 1], true)
+	# Backward pass
+	for j in range(1, _joints_resolvers.size() - 1):
+		_joints_resolvers[j].resolve_position(_joints_resolvers[j - 1], false)
+	# Return wether we are close enough to the target or not
+	return _joints_resolvers[-2].position.distance_to(_joints_resolvers[-1].position) <= _min_accepted_dist
 
 
 
-# Fetching the joints
+func _apply_to_joints() -> void:
+	for r in range(1, _joints_resolvers.size() - 2):
+		_joints_resolvers[r].set_transform(_joints_resolvers[r + 1].position)
+
+
+
 func _setup_joints() -> void:
-	_joints = []
-	for path in _joints_paths:
-		_joints.append(get_node(path))
+	var target: Spatial = get_node(_target_path)
 	
-	_joints_dists.append(0)
-	_joints_dists.append(0)
-	for i in range(1, _joints.size()):
-		var dist: float = _joints[i - 1].global_transform.origin.distance_to(_joints[i].global_transform.origin)
-		_joints_dists.append(dist)
+	# Creating the joint resolvers, and setting their bone dist
+	for i in range(_joints.size()):
+		var joint := get_node(_joints[i].target_transform)
+		var dist := 0.0
+		if i > 0:
+			var parent_pos: Vector3 = get_node(_joints[i - 1].target_transform).global_transform.origin
+			dist = joint.global_transform.origin.distance_to(parent_pos)
+		_joints_resolvers.append(JointResolver.new(joint, dist, _get_rads(_joints[i].constraint)))
+	
+	# Adding dummy root and target joints. Having a quasi-null constraint allows to correctly apply constraint on the first and last bone
+	_joints_resolvers.push_front(JointResolver.new(get_node(_joints[0].target_transform), 0.0, 0.001))
+	_joints_resolvers.append(JointResolver.new(target, 0.0, 0.001))
+
+
+
+func _get_rads(degrees: float) -> float:
+	return PI * degrees / 180.0
+
+
+
+
+# Class dedicated to do the whole joint resolvine part. There is one per joint, plus two dummies
+class JointResolver:
+	var _joint: Spatial
+	
+	# Accessing directly position and distance instead of passing by a func helps a lot to gain ms
+	var position: Vector3
+	var bone_distance: float
+	
+	var _constraint: float
+	var _forward: Vector3
 	
 	
-	# Adding the target to the joints list for the computation
-	_target = get_node(_target_path)
-	_joints.append(_target)
-	_joints_dists.append(0) # For the target
-
-
-
-
+	func _init(joint: Spatial, distance: float, constraint: float) -> void:
+		_joint = joint
+		bone_distance = distance
+		_constraint = constraint
+		_forward = -joint.global_transform.basis.z
+		refresh_position()
+	
+	
+	func refresh_position() -> void:
+		position = _joint.global_transform.origin
+	
+	
+	func resolve_position(parent_joint: JointResolver, use_parent_bone: bool) -> void:
+		var distance := parent_joint.bone_distance if use_parent_bone else bone_distance
+		_forward = parent_joint.resolve_constraint(position)
+		position = parent_joint.position + _forward * distance
+	
+	
+	func set_transform(lookat: Vector3) -> void:
+		_joint.global_transform.origin = position
+		_joint.global_transform.basis = _joint.global_transform.looking_at(lookat, Vector3.UP).basis
+	
+	
+	func resolve_constraint(source_position: Vector3) -> Vector3:
+		var direction := position.direction_to(source_position)
+		if not _constraint or not direction:
+			return direction
+		else:
+			return _clamp_direction(direction)
+	
+	
+	func _clamp_direction(direction: Vector3) -> Vector3:
+		if _forward.dot(direction) > cos(_constraint):
+			return direction
+		return _forward.rotated(_forward.cross(direction).normalized(), _constraint)
 
