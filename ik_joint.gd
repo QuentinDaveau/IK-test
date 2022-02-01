3d4245f261c89d2b1dd68118cdeaa7cd4b97816a
Quentin <daveau.quentin@hotmail.fr>
2022-02-01 21:54:49 +0100

    Add magnet

--- ik_joint.gd
+++ ik_joint.gd
@@ -10,3 +10,5 @@ export(NodePath) var target_transform
 
 # Bend angle constraints in euler angles (relative to the forward of the joint)
 export(float, 0.0, 180) var constraint = 0.0
+
+export(NodePath) var magnet
--- simple_ik.gd
+++ simple_ik.gd
@@ -7,7 +7,7 @@ Very basic IK class with simple, one-axis constraints.
 
 
 export(int) var _max_resolves: int = 4 # A resolve is a forward and backward pass
-export(float) var _min_accepted_dist: float = 0.1
+export(float) var _min_accepted_dist: float = 0.001
 export(NodePath) var _target_path
 
 # Array of IkJoint
@@ -18,12 +18,21 @@ var _joints_resolvers := []
 
 
 func _ready() -> void:
-	_setup_joints()
+	if _target_path and _joints:
+		_setup_joints()
 
 
 
 func _process(delta: float) -> void:
-	_resolve_ik()
+	if _joints_resolvers:
+		_resolve_ik()
+
+
+
+func set_target(target: Spatial) -> void:
+	_target_path = target.get_path()
+	if _target_path and _joints:
+		_setup_joints()
 
 
 
@@ -48,7 +57,7 @@ func _ik_pass() -> bool:
 	for j in range(_joints_resolvers.size() - 2, 0, -1):
 		_joints_resolvers[j].resolve_position(_joints_resolvers[j + 1], true)
 	# Backward pass
-	for j in range(1, _joints_resolvers.size() ):
+	for j in range(1, _joints_resolvers.size() - 1):
 		_joints_resolvers[j].resolve_position(_joints_resolvers[j - 1], false)
 	# Return wether we are close enough to the target or not
 	return _joints_resolvers[-2].position.distance_to(_joints_resolvers[-1].position) <= _min_accepted_dist
@@ -66,16 +75,17 @@ func _setup_joints() -> void:
 	
 	# Creating the joint resolvers, and setting their bone dist
 	for i in range(_joints.size()):
+		var magnet: Spatial = get_node(_joints[i].magnet) if _joints[i].magnet else null
 		var joint := get_node(_joints[i].target_transform)
 		var dist := 0.0
 		if i > 0:
 			var parent_pos: Vector3 = get_node(_joints[i - 1].target_transform).global_transform.origin
 			dist = joint.global_transform.origin.distance_to(parent_pos)
-		_joints_resolvers.append(JointResolver.new(joint, dist, _get_rads(_joints[i].constraint)))
+		_joints_resolvers.append(JointResolver.new(joint, dist, _get_rads(_joints[i].constraint), magnet))
 	
 	# Adding dummy root and target joints. Having a quasi-null constraint allows to correctly apply constraint on the first and last bone
-	_joints_resolvers.push_front(JointResolver.new(get_node(_joints[0].target_transform), 0.0, 0.1))
-	_joints_resolvers.append(JointResolver.new(target, 0.0, 0.1))
+	_joints_resolvers.push_front(JointResolver.new(get_node(_joints[0].target_transform), 0.0, 0.01))
+	_joints_resolvers.append(JointResolver.new(target, 0.0, 0.01))
 
 
 
@@ -85,9 +95,10 @@ func _get_rads(degrees: float) -> float:
 
 
 
-# Class dedicated to do the whole joint resolvine part. There is one per joint, plus two dummies
+# Class dedicated to do the whole joint resolving part. There is one per joint, plus two dummies
 class JointResolver:
 	var _joint: Spatial
+	var _magnet: Spatial
 	
 	# Accessing directly position and distance instead of passing by a func helps a lot to gain ms
 	var position: Vector3
@@ -97,16 +108,18 @@ class JointResolver:
 	var _constraint: float
 	
 	
-	func _init(joint: Spatial, distance: float, constraint: float) -> void:
+	func _init(joint: Spatial, distance: float, constraint: float, magnet: Spatial = null) -> void:
 		_joint = joint
 		bone_distance = distance
 		_constraint = constraint
 		forward = -joint.global_transform.basis.z
+		_magnet = magnet
 		refresh_position()
 	
 	
 	func refresh_position() -> void:
-		position = _joint.global_transform.origin
+		position = _joint.global_transform.origin if not _magnet else _magnet.global_transform.origin
+		forward = -_joint.global_transform.basis.z
 	
 	
 	func resolve_position(parent_joint: JointResolver, use_parent_bone: bool) -> void:
@@ -115,6 +128,7 @@ class JointResolver:
 		position = parent_joint.position + forward * distance
 	
 	
+	# TODO: See weight for basis
 	func set_transform(forward: Vector3) -> void:
 		_joint.global_transform.origin = position
 		_joint.global_transform.basis = _joint.global_transform.looking_at(position + forward, Vector3.UP).basis
